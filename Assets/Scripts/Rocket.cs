9f9011399788be1c6cfbc64f2ef5e5e2cba3a80d
nebo01 <eurojee.jarina@owasp.org>
2019-10-13 22:42:24 +0100

    Rocket Movements
    *New Script Added which controls the rocket's movements. just used print function to determine if controls are working.
    
    *Rigidbody added to the top rocket layer.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can trust while rotating
+        {
+            print("Space Pressed");
+        }
+        if (Input.GetKey(KeyCode.A)) // rotate left
+        {
+            print("Rotating Left");
+        } 
+        else if (Input.GetKey(KeyCode.D)) // rotate right
+        {
+            print("Rotating Right");
+        }
+    }
+}
