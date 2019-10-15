59907b3282eb0b1c48335fb4a4060840222d62a0
nebo01 <eurojee.jarina@owasp.org>
2019-10-15 22:15:14 +0100

    Version 1.2
    Update Information Version 1.2
    
    Collision Detection Added
    Player dies if the it hits the Red obstacles
    Player proceed to the next level if it landed on the Green Pad
    2 levels are now added
    Improved controls
    Improved level designs

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -9,6 +10,10 @@ public class Rocket : MonoBehaviour
     [SerializeField] float mainThrust = 100f;
 
     Rigidbody rigidBody;
+
+    enum State {Alive, Dying, Transcending};
+
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +24,59 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        //todo somewhere stop sound on death
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(state != State.Alive){return;} // break the execution 
+
+        switch (collision.gameObject.tag) // check tags on collision
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f); // wait for 1 second before loading next scene
+                break;
+            default:
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 2f);
+                break;
+        }
+    }
+
+    void LoadNextScene()
+    {
+/*        switch (state)
+        {
+            case State.Transcending:
+                print("Transcending State");
+                break;
+            case State.Dying:
+                print("Dying State");
+                break;
+            default:
+                print("No state");
+                break;
+        }*/
+
+        SceneManager.LoadScene(1); // todo allow more than 2 levels;
+    }
+    
+    void LoadFirstLevel() 
+    {
+        SceneManager.LoadScene(0); // todo allow more than 2 levels;
     }
 
     private void Thrust()
     {
-       
-
         if (Input.GetKey(KeyCode.Space)) // can trust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up * mainThrust);
