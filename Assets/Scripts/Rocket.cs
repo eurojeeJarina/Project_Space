f609fb8057749269ff524c281506b3cab118f802
nebo01 <eurojee.jarina@owasp.org>
2019-10-19 23:24:50 +0100

    Android Version
    *An android version of the game was built.
    *OnPressedDown OnpressedUp function needs to be incorporated

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] float levelLoadDelay = 2f;
+
     [SerializeField] AudioClip mainEngineSound;
     [SerializeField] AudioClip deathSound;
     [SerializeField] AudioClip levelLoadSound;
@@ -16,18 +19,31 @@ public class Rocket : MonoBehaviour
     [SerializeField] ParticleSystem deathParticles;
     [SerializeField] ParticleSystem levelLoadParticles;
 
+ /*   [SerializeField] Button leftButton;
+    [SerializeField] Button RightButton;*/
+   
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+    
 
     enum State {Alive, Dying, Transcending};
 
     State state = State.Alive;
+
+    bool buttonPressed = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+/*
+        RightButton.GetComponent<Button>();
+        leftButton.GetComponent<Button>();
+
+        leftButton.onClick.AddListener(RespondToLeftButton);
+        RightButton.onClick.AddListener(RespondToLeftButton);*/
     }
 
     // Update is called once per frame
@@ -63,7 +79,7 @@ public class Rocket : MonoBehaviour
         audioSource.Stop();
         audioSource.PlayOneShot(levelLoadSound);
         levelLoadParticles.Play();
-        Invoke("LoadNextScene", 1f); // wait for 1 second before loading next scene
+        Invoke("LoadNextScene", levelLoadDelay); // wait for 1 second before loading next scene
     }
 
     void StartDeathSequence()
@@ -72,7 +88,7 @@ public class Rocket : MonoBehaviour
         audioSource.Stop();
         audioSource.PlayOneShot(deathSound);
         deathParticles.Play();
-        Invoke("LoadFirstLevel", 2f);
+        Invoke("LoadFirstLevel", levelLoadDelay);
     }
 
     void LoadNextScene()
@@ -100,7 +116,7 @@ public class Rocket : MonoBehaviour
 
     private void RespondToThrustInput()
     {
-        if (Input.GetKey(KeyCode.Space)) // can trust while rotating
+        if (Input.GetButton("Fire1")) // can trust while rotating
         {
             ApplyThrust();
         }
@@ -112,7 +128,7 @@ public class Rocket : MonoBehaviour
 
     void ApplyThrust()
     {
-        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); // TODO: try & comeback solve the Time.deltaTime; problem.
 
         if (!audioSource.isPlaying) // so it doesn't layer
         {
@@ -139,4 +155,5 @@ public class Rocket : MonoBehaviour
 
         rigidBody.freezeRotation = false; // resume physics control of rotation
     }
+
 }
